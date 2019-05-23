155cec61bf2534804d081de0a2c2675f1f5dadaf
Victor Gois Vieira <victormais08@gmail.com>
2019-05-23 18:29:34 -0300

    route

--- start/routes.js
+++ start/routes.js
@@ -3,12 +3,12 @@
 const Route = use('Route')
 
 Route.post('/api/sessions', 'SessionController.store').validator('Session')
-Route.resource('/api/client', 'ClientController')
-Route.resource('/api/address', 'AddressController')
 
 Route.group(() => {
   Route.resource('/api/user_type', 'UserTypeController')
   Route.resource('/api/plan', 'PlanController')
+  Route.resource('/api/client', 'ClientController')
+  Route.resource('/api/address', 'AddressController')
   Route.resource('/api/user', 'UserController')
   Route.resource('/api/remuneration', 'RemunerationController')
   Route.resource('/api/sale', 'SaleController')
