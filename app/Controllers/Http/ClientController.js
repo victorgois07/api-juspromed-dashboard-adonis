8a8006765757928c0f17a60ed2fe7183b93e6279
Victor Gois Vieira <victormais08@gmail.com>
2019-05-24 10:35:16 -0300

    Teste Resquisição

--- app/Controllers/Http/ClientController.js
+++ app/Controllers/Http/ClientController.js
@@ -2,12 +2,12 @@
 
 const axios = require('axios')
 const Env = use('Env')
-const CircularJSON = require('circular-json')
+const req = require('request')
 
 class ClientController {
   constructor () {
     this.req = axios.create({
-      baseURL: 'https://sandbox-app.vindi.com.br:443/api/v1',
+      baseURL: Env.get('BASEURL'),
       headers: {
         Authorization: `Basic ${
           Env.get('NODE_ENV') === 'development'
@@ -36,14 +36,34 @@ class ClientController {
             reject(error)
           })
       } else {
-        this.req
+        const options = { method: 'GET',
+          url: 'https://sandbox-app.vindi.com.br:443/api/v1/customers',
+          qs: { sort_by: 'created_at', sort_order: 'desc' },
+          headers:
+            { 'cache-control': 'no-cache',
+              Connection: 'keep-alive',
+              'accept-encoding': 'gzip, deflate',
+              Host: 'sandbox-app.vindi.com.br:443',
+              'Postman-Token': 'd17cf374-4d15-49d2-b67e-8b283a14eebf,eb33579c-ca55-4d3c-96b2-6ca60a91d52e',
+              'Cache-Control': 'no-cache',
+              Accept: '*/*',
+              'User-Agent': 'PostmanRuntime/7.13.0',
+              Authorization: 'Basic X3hROHFjbkRHMy1JNE1aZXZaRjJMSmpJNGh1NUZOTkNhV2x0OWp6amlLazo=' } }
+
+        req(options, function (error, response, body) {
+          if (error) throw new Error(error)
+          console.log(body)
+          resolve(JSON.parse(body))
+        })
+
+        /* this.req
           .get('/customers?sort_by=created_at&sort_order=desc')
           .then(async data => {
             resolve(CircularJSON.stringify(data.data.customers))
           })
           .catch(error => {
             reject(error)
-          })
+          }) */
       }
     })
   }
